8d16086d4b73e7795e936bfae0eb0cedc39df980
Your Namec <MamRadZelvyy>
2023-11-03 09:22:57 +0100

    first commit

---
+++ res/js/main.js
@@ -0,0 +1,51 @@
+//         params - parametry
+/* function tomas(item, item2){
+    console.log("Nesu: " + item + "," + item2);
+}
+
+//      args - argumenty
+tomas("mys", "pocitac");
+tomas("notebook", "monitor");
+
+
+
+// arrow funkce 2. zpusob
+const ttomas = (item, item2) => {
+    console.log(`Nesu: ${item}, ${item2}`);
+}
+ttomas("mys", "pocitac");
+ttomas("notebook", "monitor");
+*/
+
+// $ - shift + 4 (nad písmenem R)
+// {} - shift + prava klavesa od pismene P
+// () - shift + 9 (nad pismenem O)
+// [] - prava klase od pismene P
+
+/*function play (song, artist) {
+console.log(`Playing: ${song} by ${artist}`)
+}
+
+
+play("Never gonna give you up", "Rick Astley");
+playMusic("HAFO","Frank wild")*/
+
+// alt+shift+f = formátování
+
+/*function sum(a, b) {
+  return a + b;
+}
+
+let resultA = sum(5, 4);
+let resultB = sum(7, 4);
+let result = sum(resultA, resultB);
+console.log(result);
+
+
+
+const ssum = (a, b) => a + b;
+
+let rresultA = sum(5, 4);
+let rresultB = sum(7, 4);
+let rresult = sum(rresultA, rresultB);
+console.log(rresult);*/
